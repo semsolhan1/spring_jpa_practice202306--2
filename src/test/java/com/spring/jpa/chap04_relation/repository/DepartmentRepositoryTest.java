e573b3af4278c7cf76db869abb13477f57269c88
KOOBONSIK <megatron1234@naver.com>
2023-06-16 17:14:53 +0900

    # 4. JPA  단방향 맵핑(연관관계)

--- src/test/java/com/spring/jpa/chap04_relation/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/spring/jpa/chap04_relation/repository/DepartmentRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.spring.jpa.chap04_relation.entity;
+package com.spring.jpa.chap04_relation.repository;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -10,6 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 @Rollback(false)
-class EmployeeTest {
+class DepartmentRepositoryTest {
 
 }
