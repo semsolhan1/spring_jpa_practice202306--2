096d7fe82b72b673adc3eca2f8652f361137741b
KOOBONSIK <megatron1234@naver.com>
2023-06-16 16:25:48 +0900

    # 2. JPA Repository 연습(QueryMethod, JPQL)

---
+++ src/test/java/com/spring/jpa/chap04_relation/entity/EmployeeTest.java
@@ -0,0 +1,15 @@
+package com.spring.jpa.chap04_relation.entity;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class EmployeeTest {
+
+}
